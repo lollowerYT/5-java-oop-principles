1ab6e462b63186f644004e6d069e384d521cd2d8
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-10-13 23:31:01 +0500

    Update Task02Main.java

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,7 +1,6 @@
 package com.example.task02;
 
 public class Task02Main {
-
     private static final Item ITEM1 = new Item("Товар 1", 10);
     private static final Item ITEM2 = new Item("Товар 2", 20);
     private static final Item ITEM3 = new Item("Товар 3", 30);
@@ -10,6 +9,8 @@ public class Task02Main {
     private static final Item ITEM6 = new Item("Товар 6", 60);
 
     public static void main(String[] args) {
+        // Тестирование обычного счета
+        System.out.println("Тестирование обычного счета:");
         Bill bill = new Bill();
         bill.add(ITEM1, 10);
         bill.add(ITEM3, 3);
@@ -17,5 +18,41 @@ public class Task02Main {
         System.out.println(bill);
         bill.add(ITEM3, 3);
         System.out.println(bill);
+
+        System.out.println("\n----------------------------------------\n");
+
+        // Тестирование счета со скидкой
+        System.out.println("Тестирование счета со скидкой (15%):");
+        DiscountBill discountBill = new DiscountBill(15);
+
+        // Добавляем товары
+        discountBill.add(ITEM2, 2);
+        discountBill.add(ITEM4, 1);
+        discountBill.add(ITEM5, 3);
+
+        // Выводим полную информацию
+        System.out.println("Исходная сумма: " + discountBill.getPrice());
+        System.out.println("Процент скидки: " + discountBill.getDiscount());
+        System.out.println("Сумма скидки: " + discountBill.getDiscountAmount());
+        System.out.println("Итоговая сумма со скидкой: " + discountBill.getTotalAmount());
+
+        // Выводим полный счет
+        System.out.println("\nПолный счет:");
+        System.out.println(discountBill);
+
+        // Добавляем еще товары и проверяем изменения
+        System.out.println("\nПосле добавления новых товаров:");
+        discountBill.add(ITEM1, 5);
+        discountBill.add(ITEM6, 2);
+
+        // Выводим обновленную информацию
+        System.out.println("Исходная сумма: " + discountBill.getPrice());
+        System.out.println("Процент скидки: " + discountBill.getDiscount());
+        System.out.println("Сумма скидки: " + discountBill.getDiscountAmount());
+        System.out.println("Итоговая сумма со скидкой: " + discountBill.getTotalAmount());
+
+        // Выводим обновленный счет
+        System.out.println("\nОбновленный счет:");
+        System.out.println(discountBill);
     }
 }
