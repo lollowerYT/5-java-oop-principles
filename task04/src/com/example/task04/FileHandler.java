209f6b3a19711aab0523ad4e03634192f043ca2a
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-11-24 17:02:02 +0500

    Update FileHandler.java

--- task04/src/com/example/task04/FileHandler.java
+++ task04/src/com/example/task04/FileHandler.java
@@ -4,18 +4,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileHandler implements MessageHandler {
-    private String fileName;
 
-    public FileHandler(String fileName) {
-        this.fileName = fileName;
+    private final String filename;
+
+    public FileHandler(String filename) {
+        this.filename = filename;
     }
 
     @Override
-    public void log(String message) {
-        try (FileWriter writer = new FileWriter(fileName, true)) {
-            writer.write(message + "\n");
+    public void handle(String message) {
+        try (FileWriter fw = new FileWriter(filename, true)) {
+            fw.write(message + System.lineSeparator());
         } catch (IOException e) {
-            System.err.println("Ошибка записи в файл: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
