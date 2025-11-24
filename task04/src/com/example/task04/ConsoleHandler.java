233d1cad9dc593db2bfe86691fe91aa3508b624e
lollowerYT <77741489+lollowerYT@users.noreply.github.com>
2025-11-24 16:59:45 +0500

    Update ConsoleHandler.java

--- task04/src/com/example/task04/ConsoleHandler.java
+++ task04/src/com/example/task04/ConsoleHandler.java
@@ -2,10 +2,7 @@ package com.example.task04;
 
 public class ConsoleHandler implements MessageHandler {
     @Override
-    public void handle(String Message) {
-        System.out.println(Message);
+    public void handle(String message) {
+        System.out.println(message);
     }
-
-    @Override
-    public void close() { }
 }
